7e1f9b5ae596ec048e33096cb620ed216ad28224
Aleksandr Razumov <ernado@go-faster.org>
2025-07-24 20:57:57 +0300

    chore(gen): upd

--- internal/ent/runtime/runtime.go
+++ internal/ent/runtime/runtime.go
@@ -2,7 +2,7 @@
 
 package runtime
 
-// The schema-stitching logic is generated in github.com/cydev/cgbot/internal/ent/runtime.go
+// The schema-stitching logic is generated in github.com/ernado/lupanarbot/internal/ent/runtime.go
 
 const (
 	Version = "v0.14.4"                                         // Version of ent codegen.
